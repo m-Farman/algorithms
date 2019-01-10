056e2b8da06eca171ba087b9b6c6d52cec447eb4
m-Farman <farmanmohd8@gmail.com>
2019-01-10 17:31:29 +0530

    Rod Cutting Problem

--- src/dynamicprogramming/RodCuttingProblem.java
+++ src/dynamicprogramming/RodCuttingProblem.java
@@ -8,6 +8,7 @@ public class RodCuttingProblem {
 		int[] price = { 1, 5, 8, 9, 10, 17, 17, 20 };
 		int value = getMaxProfit(size, price, 8);
 		System.out.println(value);
+		System.out.println("Sizes are : " + maxProfitSizes(size, price, 8));
 		System.out.println("------new price-------");
 		price = new int[] { 3, 5, 8, 9, 10, 17, 17, 20 };
 		System.out.println(getMaxProfit(size, price, 8));
@@ -32,10 +33,45 @@ public class RodCuttingProblem {
 		return cost[rodSize];
 	}
 
+	private static String maxProfitSizes(int[] size, int[] price, int rodSize) {
+
+		int[] cost = new int[rodSize + 1];
+		int[] sUsed = new int[rodSize + 1];
+
+		int max = Integer.MIN_VALUE;
+		for (int i = 1; i <= rodSize; i++) {
+			max = Integer.MIN_VALUE;
+			for (int j = 0; j < size.length; j++) {
+				if (size[j] > i) {
+					break;
+				}
+				if (max < price[j] + cost[i - size[j]]) {
+					max = price[j] + cost[i - size[j]];
+					sUsed[i] = size[j];
+				}
+			}
+			cost[i] = max;
+		}
+
+		int index = rodSize;
+		StringBuilder sb = new StringBuilder();
+		while (index > 0) {
+			sb.append(sUsed[index]);
+			index -= sUsed[index];
+			if (index > 0) {
+				sb.append(",");
+			}
+		}
+
+		return sb.toString();
+	}
+
 }
 
-/*output
+/*
+ * output 
 22
-------new price data-------
+Sizes are : 2,6
+------new price-------
 24
- * */
+ */
