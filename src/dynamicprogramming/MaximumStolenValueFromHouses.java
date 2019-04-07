8bb400cbb2722bbfeef6f4bc823083bf6092057c
m-Farman <farmanmohd8@gmail.com>
2019-04-07 23:14:20 +0530

    Maximum Stolen Value From Houses

---
+++ src/dynamicprogramming/MaximumStolenValueFromHouses.java
@@ -0,0 +1,36 @@
+package dynamicprogramming;
+
+//https://www.geeksforgeeks.org/find-maximum-possible-stolen-value-houses/
+public class MaximumStolenValueFromHouses {
+
+	public static void main(String[] args) {
+		int arr[] = { 6, 7, 1, 3, 8, 2, 4 };
+
+		int output = find(arr);
+		System.out.println(output);
+	}
+
+	private static int find(int[] arr) {
+
+		if (arr.length == 0) {
+			return 0;
+		}
+
+		if (arr.length == 1) {
+			return arr[0];
+		}
+		if (arr.length == 2) {
+			return Math.max(arr[0], arr[0]);
+		}
+
+		int[] value = new int[arr.length];
+		value[0] = arr[0];
+		value[1] = Math.max(arr[0], arr[0]);
+
+		for (int i = 2; i < value.length; i++) {
+			value[i] = Math.max(arr[i] + value[i - 2], value[i - 1]);
+		}
+
+		return value[arr.length - 1];
+	}
+}
